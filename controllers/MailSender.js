fbdbb4e8661a6e0aa33c6bdb8b344eee85dc2de4
ANASS OULED BEN TAHAR <Anass.taher@gmail.com>
2022-02-09 01:29:46 +0100

    reset password

--- controllers/MailSender.js
+++ controllers/MailSender.js
@@ -1,32 +1,44 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
-async function mail(receiver, subject, text,html= "") {
+async function mail(receiver, subject, text, html = '') {
+  const data = {
+    user: process.env.EMAIL,
+    pass: process.env.EMAILPASSWORD,
+  };
+  console.log(data);
 
-    let mailTransporter = nodemailer.createTransport({
-        service: 'gmail',
-        // secure: false, 
-        // requireTLS: true,
-        auth: {
-            user: 'pmp.trainingma@gmail.com',
-            pass: 'PMP-Training@123'
-        }
-    });
+  let mailTransporter = nodemailer.createTransport({
+    service: 'gmail',
+    // secure: false,
+    // requireTLS: true,
+    auth: data,
+  });
 
-    let mailDetails = {
-        from: 'pmp.trainingma@gmail.com',
-        to: receiver,
-        subject: subject, // Subject line
-        text: text, // plain text body
-        html: html
-        };
+//   var mailTransporter = nodemailer.createTransport({
+//     host: "smtp.mailtrap.io",
+//     port: 2525,
+//     auth: {
+//       user: "4d48f69c1d8d9d",
+//       pass: "f4a4568c698bf7"
+//     }
+//   });
 
-    mailTransporter.sendMail(mailDetails, function (err, data) {
-        if (err) {
-            console.log('Une erreur est produite', err);
-        } else {
-            console.log('Email envoye avec success', data);
-        }
-    });
+
+  let mailDetails = {
+    from: process.env.EMAIL,
+    to: receiver,
+    subject: subject, // Subject line
+    text: text, // plain text body
+    html: html,
+  };
+
+  mailTransporter.sendMail(mailDetails, function (err, data) {
+    if (err) {
+      console.log('Une erreur est produite', err);
+    } else {
+      console.log('Email envoye avec success', data);
+    }
+  });
 }
 
-module.exports = mail
+module.exports = mail;
